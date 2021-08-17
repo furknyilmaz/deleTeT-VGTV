cec60ff4512705d0cb5088b3dbebd580ef922bdf
Bahadır TIRAŞ <otiras.mdbf17@iste.edu.tr>
2021-08-18 00:40:16 +0300

    anasayda tasarlandı, Login giriş ayarlandı

---
+++ lib/pages/CompanyScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CompanyScreen extends StatelessWidget {
+  const CompanyScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text('Burası Şirketler Screen'),
+    ));
+  }
+}
